c2b0c3962af3bacefb0d547cb340dccc488ddbae
dev-rahim <rahimuddin5915@gmail.com>
2022-02-14 21:59:35 +0600

    paramitar error

---
+++ input-error.js
@@ -0,0 +1,15 @@
+function add( num1, num2 ) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+const firstTotal = add( 63, 85 );
+// console.log( firstTotal );
+
+function multiply( num1, num2 ) {
+    const sum = num1 * num2;
+    return sum;
+}
+const firstResult = multiply( 6, 5 );
+// const firstResult = multiply( 6 * 5 );
+console.log( firstResult );
